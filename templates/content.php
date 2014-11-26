e2f28c49eb1b173e250591dce0e6cf5dc05032ac
Ed Atrero <eatrero@gmail.com>
2014-11-26 13:59:07 -0800

    fix menu

--- templates/content.php
+++ templates/content.php
@@ -3,12 +3,16 @@
 		<?php $hasFeaturedImage = has_post_thumbnail();?>
 		<?php	$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>		
 		<?php if($feat_image): ?>
-			<div class="col-sm-12 col-xs-12 lead-image-container">
-				<a href="<?php the_permalink(); ?>"><img class="img-responsive lead-dark" src="<?php echo $feat_image; ?>"/></a>
-			</div>
+			<a href="<?php the_permalink(); ?>">
+			<figure class="col-sm-12 col-xs-12 lead-image-container">
+				<img class="img-responsive lead-dark" src="<?php echo $feat_image; ?>"/>
+				  <figcaption>
+		    	  <h2 class="entry-title"><?php the_title(); ?></h2>
+		    	</figcaption>
+			</figure>
+			</a>
 		<?php endif ?>
 
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php // get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
